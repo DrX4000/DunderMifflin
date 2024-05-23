89aea3e64af11bd99ef1a07b349b63f1a3ad8c1a
Daroux <hugo.daroux@etu.estia.fr>
2024-05-23 14:57:16 +0200

    Ch Env

--- model/php/env_settings.php
+++ model/php/env_settings.php
@@ -1,10 +1,10 @@
 <?php
 
 // if we are in the local environment
-$host = "tai_app_2023_2024_mouse";
-$dbname = "tai_app_2023_2024_mouse";
-$user = "tai_app_2023_2024_mouse";
-$pwd = "RLWNNSO3OO";
+$host = "localhost";
+$dbname = "niveaudestock";
+$user = "root";
+$pwd = "";
 
 // if we are on the server
 if (file_exists("/DunderMifflin")) {
