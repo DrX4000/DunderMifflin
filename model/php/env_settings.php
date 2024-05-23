0c9d9f04fef9d58996c9c2449bc0e92df1ec19a3
Daroux <hugo.daroux@etu.estia.fr>
2024-05-23 14:54:14 +0200

    Ch

--- model/php/env_settings.php
+++ model/php/env_settings.php
@@ -9,9 +9,9 @@ $pwd = "";
 // if we are on the server
 if (file_exists("/DunderMifflin")) {
     $host = "dundermifflin";
-    $dbname = "niveaudestock";
-    $user = "root";
-    $pwd = "";
+    $dbname = "tai_app_2023_2024_mouse";
+    $user = "tai_app_2023_2024_mouse";
+    $pwd = "RLWNNSO3OO";
 }
 
 
